0ff9feb7d5dd1cef37d560b888bde29f682d0af8
rohanwebdev <kumarrohan37732@gmail.com>
2025-07-09 11:34:39 +0530

    Practiced array basic questions

--- daily_modules/arrays.py
+++ daily_modules/arrays.py
@@ -1,6 +1,6 @@
 "WAF to print array"
 # def arr():
-#     arr = [1, 2, 3, 4, 5]
+#     arr = [1, 33, 3, 4, 5]
 #     print(arr)
 # arr()
 
@@ -34,3 +34,106 @@
 # num_list, summ= arraySum()
 # print(f"The array is {num_list} sum of array is {summ}")
 
+
+"Write a function to reverse a list without using the built-in reverse() method."
+# def reverse_arr(arr):
+#     revarr = []
+#     for i in range(len(arr)-1, -1, -1):
+#         revarr.append(arr[i])
+#     return revarr
+
+# input_str = input("Enter numbers separated by space: ")
+# num_list = [int(i) for i in input_str.split()]
+# revarr = reverse_arr(num_list)
+# print(f"The original array: {num_list} \nThe reversed array: {revarr}")
+
+"OR"
+
+# def reverse_arr(arr):
+#     return arr[::-1]
+
+# input_str = input("Enter numbers separated by space: ")
+# num_list = [int(i) for i in input_str.split()]
+# revarr = reverse_arr(num_list)
+# print("Reversed array:", revarr)
+
+
+
+"Given an array, find the maximum and minimum number in it."
+# def min_max(arr):
+#     maxx = max(arr)
+#     minn = min(arr)
+#     return minn, maxx
+# input_str = input("Enter numbers separated by space: ")
+# num_list = [int(i) for i in input_str.split()]
+# minn, maxx = min_max(num_list)
+# print(f"The MAX elem in the array is \"{maxx}\"\nThe  MIN elem in array is \"{minn}\"")
+
+
+# a = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# a.sort()
+# print(a)
+
+
+# a = [100, 50, 65, 82, 23]
+# a.sort()
+# print(a)
+
+
+# a = [100, 50, 65, 82, 23]
+# a.sort(reverse=True)
+# print(a)
+
+
+# def myfunc(n):
+#     return abs(n - 50)
+
+# a = [100, 50 , 65, 82, 23]
+# a.sort(key = myfunc)
+# print(a)
+# a = ["banana", "Orange", "Kiwi", "cherry"]
+# a.sort()
+# print(a)
+
+
+# a = ["banana", "Orange", "Kiwi", "cherry"]
+# a.reverse()
+# print(a)
+
+
+# a = ["hello", "world", "today"]
+# b = a.copy()
+# b[0] = "hi"
+# print(b)
+# print(a)
+
+
+# a = ["hello", "world", "today"]
+# a = list(a)
+# print(a)
+
+
+# a = ["hello", "world", "today"]
+# a = a[:]
+# print(a)
+
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+
+# list3 = list1 + list2
+# print(list3)
+
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+# for i in list2:
+#     list1.append(i)
+# print(list1)
+# print(list2)
+
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+# list1.extend(list2)
+# print(list1)
