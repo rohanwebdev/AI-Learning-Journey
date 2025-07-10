a0dbe5bdae741da5c01350ca48ec0f0b4bef9b97
rohanwebdev <kumarrohan37732@gmail.com>
2025-07-10 22:49:25 +0530

    completed  level 1

--- daily_modules/arrays.py
+++ daily_modules/arrays.py
@@ -220,3 +220,90 @@
 # number = int(input("Enter the length of the list: "))
 # result = list(number)
 # print(f"The size of list is {number} and appended list is {result}")
+
+
+"Sort the list using any sorting logic like bubble sort or selection sort (don't use sort() or sorted())."
+"bubble sort"
+
+# def bubble_sort(arr):
+#     n = len(arr)
+#     for i in range(n):
+#         # Track if any swap was made
+#         swapped = False
+#         for j in range(0, n - i - 1):
+#             if arr[j] > arr[j + 1]:
+#                 # Swap
+#                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+#                 swapped = True
+#         # If no swaps, list is sorted
+#         if not swapped:
+#             break
+#     return arr
+
+
+
+"selection sort"
+# def selection_sort(arr):
+#     n = len(arr)
+#     for i in range(n):
+#         min_idx = i
+#         for j in range(i+1, n):
+#             if arr[j] < arr[min_idx]:
+#                 min_idx = j
+#         # Swap minimum with current index
+#         arr[i], arr[min_idx] = arr[min_idx], arr[i]
+#     return arr
+
+
+"A list is a palindrome if it reads the same forward and backward. Check if a given list is a palindrome."
+# def palindrome(listt):
+#     rev_list = listt[::-1]
+#     if(rev_list == listt):
+#         return "Yes"
+#     else:
+#         return "No"
+# str_list = input("Enter the elems of the list separated by spaces: ")
+# num_list = [int(i) for i in str_list.split()]
+# result = palindrome(num_list)
+# print(f"{result}! palindrome list.")
+
+
+"Multiply each element in the array by a given number (e.g., 2)."
+# def multiplied_List(listt, numm):
+#     multiplied_list = []
+#     for i in range(len(listt)):
+#         elem = listt[i]*numm 
+#         multiplied_list.append(elem)
+#     return multiplied_list
+# str_list = input("Enter the elems of the list separated by spaces: ")
+# num = int(input("Enter the num to be multiplied: "))
+# num_list = [int(i) for i in str_list.split()]
+# result = multiplied_List(num_list, num)
+# print(f"Original list {num_list}\nMultiplied list by {num}: {result}")
+
+
+"Take two arrays, merge them into one, and sort the resulting array."
+# def mergeSort(list1, list2):
+#     list3 = []
+#     list3 = list1  + list2
+#     originalList = list3.copy()
+#     n = len(list3)
+#     for i in range(n):
+#         # Track if any swap was made
+#         swapped = False
+#         for j in range(0, n - i - 1):
+#             if list3[j] > list3[j + 1]:
+#                 # Swap
+#                 list3[j], list3[j + 1] = list3[j + 1], list3[j]
+#                 swapped = True
+#         # If no swaps, list is sorted
+#         if not swapped:
+#             break
+#     return list3, originalList
+    
+# str_list1 = input("Enter the elems of the list1 separated by spaces: ")
+# num_list1 = [int(i) for i in str_list1.split()]
+# str_list2 = input("Enter the elems of the list2 separated by spaces: ")
+# num_list2 = [int(i) for i in str_list2.split()]
+# list3, originalList = mergeSort(num_list1, num_list2)
+# print(f"First list: {num_list1}\nSecond List: {num_list2}\nCombined added: {originalList}\nSorted list : {list3}")
